5db178eac41822cdddf8ce85c16ea8831e1c9593
DavydovDenys <denvanrain@gmail.com>
2020-08-28 23:28:52 +0300

    add component lifecycle methods to Persons.js

--- src/cmponents/Persons/Persons.js
+++ src/cmponents/Persons/Persons.js
@@ -3,6 +3,34 @@ import Person from './Person/Person'
 
 
 class Persons extends Component {
+  // static getDerivedStateFromProps(props,state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+  
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate')
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  // componentWillUpdate() {
+    
+  // }
+
+  componentDidUpdate(prevProps, prevState, sapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log('snapshot');
+  }
+
   render() {
     console.log('[Persons.js] rendering...');
   
